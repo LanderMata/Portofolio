04ac02b14ee4760d3b8353399535bee48ceaf6c6
Lander <lander.mata.pascual@zabalburu.org>
2024-02-19 09:15:42 +0100

    Añadido temperaturas

---
+++ Grados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturas
+{
+    public static class Grados
+    {
+        //Métodos estáticos para convertir de grados centígrados a farenheit
+        public static double Cent_a_Far(double gradosC)
+        {
+
+            return (gradosC * 9 / 5) + 32;
+
+        }
+        //Métodos estáticos para convertir de grados farenheit a centígrados
+        public static double Far_a_Cent(double gradosF)
+        {
+
+            return (gradosF - 32) * 5 / 9;
+
+        }
+    }
+}
